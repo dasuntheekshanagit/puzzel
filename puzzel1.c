ddcc35e9f06d90d8b69fc27c7af7e0c90b86d6fd
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2022-12-20 08:45:07 +0530

    Move code lines to getInput function to get user input and store them in global 2d arrays

--- puzzel1.c
+++ puzzel1.c
@@ -3,23 +3,39 @@
 
 #define SIZE 256
 
+char grid[SIZE][SIZE]={0},word[SIZE][SIZE]={0};
+
+void getInput();
+
 int main(){
-    char grid[SIZE][SIZE]={0},word[SIZE][SIZE]={0},line[SIZE];
+
+    getInput();
+
+    return 0;
+}
+
+void getInput(){
+    char line[SIZE]= {0};
     int pointer = 0;
 
     printf("Grid: \n");
+
     while (scanf("%[^\n]%*c",line) != 0){
         strcat(grid[pointer],line);
-        printf("%s\n",grid[pointer]);
+        //printf("%s\n",grid[pointer]);
         pointer++;
     }
+
     scanf("%*c",line);
+
     pointer = 0;
     printf("Word: \n");
+
     while (scanf("%[^\n]%*c",line) != 0){
         strcat(word[pointer],line);
-        printf("%s\n",word[pointer]);
+        //printf("%s\n",word[pointer]);
         pointer++;
     }
-    return 0;
+
+    return ;
 }
