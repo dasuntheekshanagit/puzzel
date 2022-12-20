bbe421934b1c9a6b664f455345a486326e95ec76
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2022-12-20 09:15:17 +0530

    Made new function cladded validate to check row by row in the grid. Also it will calculate the row and columns. input_validation function was mergerd to the code.

--- puzzel1.c
+++ puzzel1.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
 #define SIZE 256
 
 char grid[SIZE][SIZE]={0},word[SIZE][SIZE]={0};
+int row,col;
 
 void getInput();
+int input_validity(char *,int);
+int validate();
 
 int main(){
 
     getInput();
-
+    int i = validate();
+    printf("%d %d\n",row,col);
     return 0;
 }
 
@@ -18,7 +23,7 @@ void getInput(){
     char line[SIZE]= {0};
     int pointer = 0;
 
-    printf("Grid: \n");
+    //printf("Grid: \n");
 
     while (scanf("%[^\n]%*c",line) != 0){
         strcat(grid[pointer],line);
@@ -29,7 +34,7 @@ void getInput(){
     scanf("%*c",line);
 
     pointer = 0;
-    printf("Word: \n");
+    //printf("Word: \n");
 
     while (scanf("%[^\n]%*c",line) != 0){
         strcat(word[pointer],line);
@@ -38,4 +43,35 @@ void getInput(){
     }
 
     return ;
+}
+
+int input_validity(char puzzle[],int len){
+    for(int j=0;j<len;j++){
+            if (isalpha(puzzle[j]) || puzzle[j]=='#' || puzzle[j] == '*' ){
+                continue;
+            }
+            else{
+                return 0;
+            }
+            }
+        return 1;
+}
+
+int validate(){
+    int len;
+    col = strlen(grid[0]);
+
+    do{
+        len = strlen(grid[row]);
+        int sts = input_validity(grid[row],len);
+        if (!sts){
+            printf("INVALID INPUT\n");
+            return sts;
+        }
+
+        row++;
+    }
+    while (len > 0);
+
+    return 1;
 }
