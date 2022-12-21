b6845a358c52b6edbf78a109e266a23eed703296
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2022-12-21 19:06:55 +0530

    Test case 6 added

--- grader.py
+++ grader.py
@@ -11,7 +11,7 @@ def filesize():
     
 def main():
     FILE = filesize()
-    for i in range(5):
+    for i in range(6):
         puzzle  = open("puzzels/"+"puzzel"+ str(i+1)+".txt",'r')
         answer = open("answer/"+"answer"+ str(i+1)+".txt",'r')
         
